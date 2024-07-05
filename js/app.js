d9447dd0aa41f5ba420432e18dabba4d8f8964ef
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-05 21:47:59 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -411,22 +411,3 @@ function initializeMap() {
 
     infowindow.open(map, marker);
 }
-
-/*
-//남성 & 여성 선택
-function selectSex(content) {
-  var male = document.getElementById("male");
-  var female = document.getElementById("female");
- 
-  // 내용 숨김
-  male.style.display = "none";
-  female.style.display = "none";
-
-  // 선택한 내용 보이기
-  if (content === "male") {
-    male.style.display = "block";
-  } else if (content === "female") {
-    female.style.display = "block";
-
-}
-*/
