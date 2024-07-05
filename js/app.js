283d33a4064ed09612c856701b5ebea66670a8cd
gubaeki <110735099+gubaeki@users.noreply.github.com>
2024-07-05 16:10:45 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -417,16 +417,14 @@ function initializeMap() {
 function selectSex(content) {
   var male = document.getElementById("male");
   var female = document.getElementById("female");
-var service = document.getElementById("service");
   // 내용 숨김
   male.style.display = "none";
   female.style.display = "none";
-  service.style.display = "none";
-/*
+
   // 선택한 내용 보이기
   if (content === "male") {
     male.style.display = "block";
   } else if (content === "female") {
     female.style.display = "block";
-  }*/
+
 }
